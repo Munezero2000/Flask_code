2bad985e47d0af4d81f68905d285ffc837fe068b
Munezero <muneangechaste@gmail.com>
2023-02-04 13:34:03 +0200

    Initial commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+# Create a Flask instance
+
+app = Flask(__name__)
+
+# Create a route  a decorator]
+
+
+@app.route('/')
+def Home():
+    stuff = "This a <strong>Bold</strong> text"
+    favorite_pizza = ['Four seasons', 'Vegetables Pizza', 50]
+    return render_template("index.html",
+                           stuff=stuff,
+                           favorite_pizza=favorite_pizza)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+# Create Custom Erroe Page
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal Server Error
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
